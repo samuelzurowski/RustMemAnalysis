ef7e2334bc418b4543be1c1a6cfb5a0e62354853
Samuel Zurowski <samuelzurowski@gmail.com>
2023-01-09 16:00:15 -0800

    Show main tree

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-// use winapi::shared::minwindef::LPCVOID;
-// // use winapi::um::memoryapi::{
-// //     VirtualAlloc, VirtualFree, VirtualLock, VirtualProtect, VirtualQuery, VirtualUnlock};
-// use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
-// use std;
-// use winapi::um::memoryapi::VirtualQueryEx;
-// use winapi::um::{processthreadsapi, sysinfoapi};
-// use winapi::um::sysinfoapi::{GetNativeSystemInfo, SYSTEM_INFO};
-// use winapi::um::winnt::{PVOID, MEMORY_BASIC_INFORMATION, MEM_RELEASE, MEM_RESERVE, MEM_COMMIT, PROCESS_QUERY_INFORMATION,PROCESS_VM_READ, MEM_PRIVATE, MEM_IMAGE, MEM_MAPPED};
-
-// use std::cmp::{max, min};
-// use std::io;
-// use std::mem::{size_of, MaybeUninit};
-// use winapi::shared::basetsd::SIZE_T;
-// use winapi::um::memoryapi::{
-//   VirtualAlloc, VirtualFree, VirtualLock, VirtualProtect, VirtualQuery, VirtualUnlock,
-// };
-
-// use winapi::{ctypes::c_void, um::winnt::MEMORY_BASIC_INFORMATION};
 use std::{ffi::c_void};
 use ptree::{TreeBuilder, print_tree};
 use windows::Win32::{System::{Threading::{OpenProcess, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ}, Memory::MEMORY_BASIC_INFORMATION}, Foundation::{HANDLE, CloseHandle}};
@@ -55,7 +36,7 @@ fn main(){
                 &mut basic_info,
                 MEM_SIZE
             );   
-            // ...
+
             if res == 0 {
                 break;
             }
@@ -72,22 +53,10 @@ fn main(){
     // Build a tree using a TreeBuilder
     let mut tree = TreeBuilder::new("Virtual Memory Regions".to_string());
 
-    let mut base_addr: usize = 0;
     for basic in basic_info_vec {
         
-
-        let info_addr = basic.AllocationBase as usize;
-
-        if info_addr != 00000000 {
-            tree.add_empty_child(format!("{:08x}", info_addr));
-        } 
-
         let range = unsafe { basic.BaseAddress.add(basic.RegionSize as usize) } as usize;
 
-        if basic.BaseAddress as usize == 00000000 {
-            continue;
-        }
-        
         tree.add_empty_child(format!("\t{:08x}-{:08x}", basic.BaseAddress as usize, range));
     }
 
